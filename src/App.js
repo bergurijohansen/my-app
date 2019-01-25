8a5beedda5f0ab9513f3c378058ccbb6857cabe3
Bergur Johansen <bergurij@gmail.com>
2019-01-25 22:42:17 +0000

    milk and stuff

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            <ul>
+              <li>
+                Milk
+              </li>
+              <li>
+                Cornflakes
+              </li>
+            </ul>
           </p>
           <a
             className="App-link"
