70a2d86fbbc54760b0c84e1c94b7b0309e4e7398
Bergur Johansen <bergurij@gmail.com>
2019-01-25 22:38:06 +0000

    Updates

---
+++ precache-manifest.cbb8c9ccc4d9b0150f7d8e14cfe62dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "d3d4031d5b9a88ff9089",
+    "url": "/my-app/static/js/main.d3d4031d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d3d4031d5b9a88ff9089",
+    "url": "/my-app/static/css/main.e6e13198.chunk.css"
+  },
+  {
+    "revision": "39a38fbde14dacea8836071deeb21bed",
+    "url": "/my-app/index.html"
+  }
+];
